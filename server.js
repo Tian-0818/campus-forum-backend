2510c32c9388e084ecf805f42d9519a9347ce763
butian <1353528619@qq.com>
2025-03-10 23:29:37 +0800

    Fix Vercel config

--- server.js
+++ server.js
@@ -16,6 +16,8 @@ mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.error('MongoDB Connection Error:', err));
 
+
+
 // 加载模型
 require('./models/User');
 require('./models/Course');
@@ -25,6 +27,11 @@ require('./models/Review');
 const userRoutes = require('./routes/userRoutes');
 const courseRoutes = require('./routes/courseRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
+
+// 示例路由
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'Hello from backend!' });
+});
 
 app.use('/api/users', userRoutes);
 app.use('/api/courses', courseRoutes);
